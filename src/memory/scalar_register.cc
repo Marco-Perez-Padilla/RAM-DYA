3bc8d8b4e920e1bf40689e15e1b51cd7cdffac99
Marco Perez Padilla <alu0101469348@ull.edu.es>
2026-02-21 18:28:57 +0000

    Doxygen comments

--- src/memory/scalar_register.cc
+++ src/memory/scalar_register.cc
@@ -21,22 +21,51 @@
 #include "scalar_register.h"
 #include "../exceptions/exceptions.h"
 
+
+/**
+ * @brief Get the value of the register as a scalar.
+ * @return The scalar value stored in the register.
+ */
 int ScalarRegister::getValue() const {
   return value_;
 }
 
+
+/**
+ * @brief Set the value of the register as a scalar.
+ * @param value The scalar value to set in the register.
+ */
 void ScalarRegister::setValue(int value) {
   value_ = value;
 }
 
+
+/**
+ * @brief Get the value at a specific index in the scalar register (not supported for scalar registers).
+ * @param index The index to access (not used since this function always throws an exception).
+ * @return This function always throws an exception since scalar registers do not support indexed access.
+ * @throw InvalidOperandException when trying to access a scalar register with an index.
+ */
 int ScalarRegister::getValueAt(int index) const {
   throw InvalidOperandException("Intento de acceso indexado a un registro escalar");
 }
 
+
+/**
+ * @brief Set the value at a specific index in the scalar register (not supported for scalar registers).
+ * @param index The index to access (not used since this function always throws an exception).
+ * @param value The value to set at the specified index (not used since this function always throws an exception).
+ * @throw InvalidOperandException when trying to access a scalar register with an index.
+ */
 void ScalarRegister::setValueAt(int index, int value) {
   throw InvalidOperandException("Intento de acceso indexado a un registro escalar");
 }
 
+
+/**
+ * @brief Check if the register is a vector register.
+ * @return false since this class represents a scalar register.
+ */
 bool ScalarRegister::isVector() const {
   return false;
 }
--- src/memory/vector_register.cc
+++ src/memory/vector_register.cc
@@ -21,16 +21,40 @@
 #include "vector_register.h"
 #include "../exceptions/exceptions.h"
 
+
+/**
+ * @brief Construct a new Vector Register object with the given data.
+ * @param data Vector of integers to initialize the register with.
+ */
 VectorRegister::VectorRegister(const std::vector<int>& data) : data_(data) {}
 
+
+/**
+ * @brief Get the value of the register as a scalar (not supported for vector registers).
+ * @return This function always throws an exception since vector registers do not support scalar access.
+ * @throw InvalidOperandException when trying to access a vector register as a scalar.
+ */
 int VectorRegister::getValue() const {
   throw InvalidOperandException("Intento de acceso escalar a un registro vectorial");
 }
 
+
+/**
+ * @brief Set the value of the register as a scalar (not supported for vector registers).
+ * @param value The scalar value to set (not used since this function always throws an exception).
+ * @throw InvalidOperandException when trying to access a vector register as a scalar.
+ */
 void VectorRegister::setValue(int value) {
   throw InvalidOperandException("Intento de acceso escalar a un registro vectorial");
 }
 
+
+/**
+ * @brief Get the value at a specific index in the vector register.
+ * @param index The index to access.
+ * @return The value at the specified index.
+ * @throw InvalidOperandException if the index is out of range.
+ */
 int VectorRegister::getValueAt(int index) const {
   if (index < 0 || index >= static_cast<int>(data_.size())) {
     throw InvalidOperandException("Índice fuera de rango en registro vectorial");
@@ -38,6 +62,13 @@ int VectorRegister::getValueAt(int index) const {
   return data_[index];
 }
 
+
+/**
+ * @brief Set the value at a specific index in the vector register.
+ * @param index The index to access.
+ * @param value The value to set at the specified index.
+ * @throw InvalidOperandException if the index is negative.
+ */
 void VectorRegister::setValueAt(int index, int value) {
   if (index < 0) {
     throw InvalidOperandException("Índice negativo en registro vectorial");
@@ -48,6 +79,11 @@ void VectorRegister::setValueAt(int index, int value) {
   data_[index] = value;
 }
 
+
+/**
+ * @brief Check if the register is a vector register.
+ * @return true since this class represents a vector register.
+ */
 bool VectorRegister::isVector() const {
   return true;
 }
