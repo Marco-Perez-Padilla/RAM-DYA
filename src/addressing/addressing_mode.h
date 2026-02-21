0c390f2a9c90358da129596cabfc0cae4296b414
Marco Perez Padilla <alu0101469348@ull.edu.es>
2026-02-21 14:36:08 +0000

    Addressing interface

---
+++ src/addressing/addressing_mode.h
@@ -0,0 +1,36 @@
+/**
+** Universidad de La Laguna
+** Escuela Superior de Ingenieria y Tecnologia
+** Grado en Ingenieria Informatica
+** Asignatura: Diseño y Analisis de Algoritmos
+** Curso: 3º
+** Practica 2: Máquina RAM
+** Autor: Marco Pérez Padilla
+** Correo: alu0101469348@ull.edu.es
+** Fecha: 21/02/2026
+
+** Archivo addressing_mode.h: Archivo de la clase interfaz de direccionamientos
+**
+** Referencias:
+**      Enlaces de interes
+
+** Historial de revisiones:
+**      21/02/2026 - Creacion (primera version) del codigo
+**/
+
+#ifndef ADDRESSING_MODE_H
+#define ADDRESSING_MODE_H
+
+#include <memory>
+
+class ExecutionContext;
+
+class AddressingMode {
+ public:
+  virtual ~AddressingMode() = default;
+  virtual int getValue(ExecutionContext& ctx, int argument) const = 0; // InvalidOperand
+  virtual int getAddress(ExecutionContext& ctx, int argument) const = 0; // InvalidOperand
+  virtual bool isWritable() const = 0;
+};
+
+#endif
---
+++ src/addressing/indirect_addressing.h
@@ -0,0 +1,33 @@
+/**
+** Universidad de La Laguna
+** Escuela Superior de Ingenieria y Tecnologia
+** Grado en Ingenieria Informatica
+** Asignatura: Diseño y Analisis de Algoritmos
+** Curso: 3º
+** Practica 2: Máquina RAM
+** Autor: Marco Pérez Padilla
+** Correo: alu0101469348@ull.edu.es
+** Fecha: 21/02/2026
+
+** Archivo indirect_addressing.h: Archivo de la clase especifica de direccionamiento indirecto
+**
+** Referencias:
+**      Enlaces de interes
+
+** Historial de revisiones:
+**      21/02/2026 - Creacion (primera version) del codigo
+**/
+
+#ifndef INDIRECT_ADDRESSING_H
+#define INDIRECT_ADDRESSING_H
+
+#include "addressing_mode.h"
+
+class IndirectAddressing : public AddressingMode {
+ public:
+  int getValue(ExecutionContext& ctx, int argument) const override;
+  int getAddress(ExecutionContext& ctx, int argument) const override;
+  bool isWritable() const override;
+};
+
+#endif
