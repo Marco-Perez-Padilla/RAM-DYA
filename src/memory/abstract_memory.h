e6161bbfc5eef14bc89ace0bd3e802a77fd2d5b5
Marco Perez Padilla <alu0101469348@ull.edu.es>
2026-02-21 13:53:46 +0000

    Memory: Abstract and register memories interfaces

---
+++ src/memory/abstract_memory.h
@@ -0,0 +1,40 @@
+/**
+** Universidad de La Laguna
+** Escuela Superior de Ingenieria y Tecnologia
+** Grado en Ingenieria Informatica
+** Asignatura: Diseño y Analisis de Algoritmos
+** Curso: 3º
+** Practica 2: Máquina RAM
+** Autor: Marco Pérez Padilla
+** Correo: alu0101469348@ull.edu.es
+** Fecha: 21/02/2026
+
+** Archivo abstract_memory.h: Archivo de la clase para l interfaz que representa la memoria
+**
+** Referencias:
+**      Enlaces de interes
+
+** Historial de revisiones:
+**      21/02/2026 - Creacion (primera version) del codigo
+**/
+
+#ifndef ABSTRACT_MEMORY_H
+#define ABSTRACT_MEMORY_H
+
+#include <memory>     // std::unique_ptr
+#include "register.h"
+
+class AbstractMemory {
+ public:
+  virtual ~AbstractMemory() = default;
+  virtual Register& getRegister(int reg) = 0; 
+  virtual bool hasRegister(int reg) const = 0;
+  int read(int reg);
+  void write(int reg, int value);
+  int read(int reg, int index);
+  void write(int reg, int index, int value);
+ protected:
+  virtual void createRegister(int reg, bool vector = false) = 0;
+};
+
+#endif
---
+++ src/memory/register_memory.h
@@ -0,0 +1,42 @@
+/**
+** Universidad de La Laguna
+** Escuela Superior de Ingenieria y Tecnologia
+** Grado en Ingenieria Informatica
+** Asignatura: Diseño y Analisis de Algoritmos
+** Curso: 3º
+** Practica 2: Máquina RAM
+** Autor: Marco Pérez Padilla
+** Correo: alu0101469348@ull.edu.es
+** Fecha: 21/02/2026
+
+** Archivo register_memory.h: Archivo de la clase que representa la memoria de registros
+**
+** Referencias:
+**      Enlaces de interes
+
+** Historial de revisiones:
+**      21/02/2026 - Creacion (primera version) del codigo
+**/
+
+#ifndef REGISTER_MEMORY_H
+#define REGISTER_MEMORY_H
+
+#include "abstract_memory.h"
+#include <unordered_map>
+#include <memory>
+
+class RegisterMemory : public AbstractMemory {
+ public:
+  RegisterMemory() = default;
+  virtual ~RegisterMemory() = default;
+  Register& getRegister(int reg) override;
+  bool hasRegister(int reg) const override;
+
+ protected:
+  void createRegister(int reg, bool vector = false) override;
+
+ private:
+  std::unordered_map<int, std::unique_ptr<Register>> registers_;
+};
+
+#endif
