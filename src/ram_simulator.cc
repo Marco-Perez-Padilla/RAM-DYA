18a4e8ac3a4622b3e114f5eb8b3266bd842e6b96
Marco Perez Padilla <alu0101469348@ull.edu.es>
2026-02-22 12:47:56 +0000

    Parsing message in main

--- src/ram_simulator.cc
+++ src/ram_simulator.cc
@@ -41,12 +41,13 @@ int main(int argc, char* argv[]) {
  * - file manager: Todos los archivos, lectura de archivos de entrada y escritura de archivos de salida
  * - exceptions: Implementación de las excepciones
  * - core: Execution context y program counter hechos
+ * - Parser: Implementar el parser completo (Adicion de funciones en help, funciones de ayuda a parser)
  */
 
  /**
   * TODO:
-  * - Parser: Implementar el parser excepto label table
   * - instruction: Implementar todos los .cc, tanto arithmetic, como control e io (asi como tmb el factory)
+  * - help: Considerar si dejar las ayudas de parser en help_functions o crear un nuevo archivo más específico. Son funciones de toup, parse, tokenize, trim
   * - help: Actualizar mensajes de ayuda y ajustar a la ejecución del programa como se pide (último paso de todos)
   * - core: Instruction executor (ejecución de instrucciones) y ram machine (núcleo del programa)
   * - addressing: Implementacion de los tres tipos de addressing 
