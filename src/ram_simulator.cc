e991b418af2186401aa5bbb88848664a92e19fd9
Marco Perez Padilla <alu0101469348@ull.edu.es>
2026-02-22 17:29:16 +0000

    Delete trash comments in main file

--- src/ram_simulator.cc
+++ src/ram_simulator.cc
@@ -7,7 +7,7 @@
 ** Práctica 2: Máquina RAM
 ** Autores: Marco Pérez Padilla, Keran Miranda González
 ** Correo: alu0101469348@ull.edu.es, alu0101485904@ull.edu.es
-** Fecha: 01/02/2026
+** Fecha: 21/02/2026
 
 ** Archivo main.cc: Programa principal que ejecuta el menú
 **
@@ -47,30 +47,3 @@ int main(int argc, char* argv[]) {
   
   return 0;
 }
-
-
-/**
- * YA IMPLEMENTADO:
- * - Todos los .h, lo siguiente es referente a los .cc
- * - tape (todos los archivos), cintas salida y entrada
- * - Parser: SOLO label table
- * - operand: Tdodos los archivos, tipos de operandos (ctes, directos, indirectos y vector para apto+)
- * - memory: Todos los archivos, memoria de la máquina RAM e implementación de los registros (y tmb la memoria de registros)
- * - file manager: Todos los archivos, lectura de archivos de entrada y escritura de archivos de salida
- * - exceptions: Implementación de las excepciones
- * - core: Execution context y program counter hechos
- * - Parser: Implementar el parser completo (Adicion de funciones en help, funciones de ayuda a parser)
- * - instruction: Implementados, incluido el executor de instrucciones
- * - addressing: Implementacion de los tres tipos de addressing YA IMPLEMENTADO INTERNAMENTE EN OPERANDOS, no es necesario separarlo
- * - core: ram machine
- * - help: Actualizar mensajes de ayuda y ajustar a la ejecución del programa como se pide (último paso de todos): Hecho
- * - help: Considerar si dejar las ayudas de parser en help_functions o crear un nuevo archivo más específico. Son funciones de toup, parse, tokenize, trim
- *      Por el momento he considerado mejor dejarlo en help_functions
- */
-
- /**
-  * TODO:
-  * 
-  * 
-  * 
-  */
